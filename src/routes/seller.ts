ec80f74e3878e41277a3933b98814a128841d83d
robin07 <robbiesingh.singh84@gmail.com>
2024-02-02 01:33:10 +0530

    retrive list of orders recieved by seller

--- src/routes/seller.ts
+++ src/routes/seller.ts
@@ -1,15 +1,32 @@
 import { Router, Request, Response } from "express";
 import { authMiddleware } from "../middleware/auth";
 import { Product } from "../db";
+import { Order } from "../db";
 
 const sellerRouter = Router();
 
 //Retrieve the list of orders recieved by seller
-sellerRouter.get("/orders", authMiddleware, (req: any, res: Response) => {
-  res.status(200).json({
-    msg: "hey",
-  });
-});
+sellerRouter.get(
+  "/orders/:seller_id",
+  authMiddleware,
+  async (req: any, res: Response) => {
+    try {
+      const orders = await Order.find(
+        {
+          sellerId: req?.params?.seller_id,
+        },
+        { sellerId: 0, orderedBy: 0 }
+      ).populate("productId", "name price");
+      res.status(200).json({
+        data: orders,
+      });
+    } catch (err) {
+      res.status(500).json({
+        msg: `Error is ${err}`,
+      });
+    }
+  }
+);
 
 //Send a list of items to create catalog for seller
 sellerRouter.post(
