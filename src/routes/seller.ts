310a9bdbdc7578177d17defc164e113646e846a3
robin07 <robbiesingh.singh84@gmail.com>
2024-02-01 11:11:03 +0530

    create-catalog-endpoint-done

--- src/routes/seller.ts
+++ src/routes/seller.ts
@@ -1,20 +1,50 @@
 import { Router, Request, Response } from "express";
 import { authMiddleware } from "../middleware/auth";
+import { Product } from "../db";
+import { Catalog } from "../db";
 
 const sellerRouter = Router();
 
 //Retrieve the list of orders recieved by seller
-sellerRouter.get(
-  "/orders",
-  authMiddleware,
-  (req: Request, res: Response) => {}
-);
+sellerRouter.get("/orders", authMiddleware, (req: any, res: Response) => {
+  res.status(200).json({
+    msg: "hey",
+  });
+});
 
 //Send a list of items to create catalog for seller
 sellerRouter.post(
-  "/create_catalog",
+  "/create_catalog/:seller_id",
   authMiddleware,
-  (req: Request, res: Response) => {}
+  async (req: Request, res: Response) => {
+    const items = req.body;
+    if (items.length > 0) {
+      try {
+        const promiseResult = await Promise.all(
+          items.map(async (item: { name: string; price: number }) => {
+            const product = await Product.create({
+              name: item.name,
+              price: item.price,
+            });
+            return await Catalog.create({
+              sellerId: req?.params?.seller_id,
+              productId: product?._id,
+            });
+          })
+        );
+        res.status(200).json({
+          data: promiseResult,
+        });
+      } catch (err) {
+        res.status(500).json({
+          msg: `Error is ${err}`,
+        });
+      }
+    }
+    res.status(200).json({
+      message: "No items have been sent",
+    });
+  }
 );
 
 export { sellerRouter };
